78ed554f98692e91c53e557175050390bf4d5e3e
ibeneliz <ibeneliz@gmail.com>
2024-02-06 00:50:12 +0530

    Best practices

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import reactImg from './assets/react-core-concepts.png';
+
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function getRandomInt(max){
@@ -5,12 +7,13 @@ function getRandomInt(max){
 }
 
 function Header() {
+  const description = reactDescriptions[getRandomInt(2)];
   return (
     <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={reactImg} alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          {reactDescriptions[getRandomInt(2)]} React concepts you will need for almost any app you are
+          {description} React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>
