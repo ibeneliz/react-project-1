a5362946a5bc31b299c25b8c22983a7f46b568d4
ibeneliz <ibeneliz@gmail.com>
2024-02-08 12:57:30 +0530

    Outputting data based on state

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import { CORE_CONCEPTS } from './data.jsx';
+import { CORE_CONCEPTS, EXAMPLES } from './data.jsx';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
+import { useState } from 'react';
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState('components');
+  
+  function handleSelect(selectedButton){
+    setSelectedTopic(selectedButton);
+  }
+
   return (
     <div>
       <Header />
@@ -16,6 +24,24 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+          <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+          <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+          <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+          <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+        </section>
+        <div id="tab-content">
+          <h3>{EXAMPLES[selectedTopic].title}</h3>
+          <p>{EXAMPLES[selectedTopic].description}</p>
+          <pre>
+            <code>
+              {EXAMPLES[selectedTopic].code}
+            </code>
+          </pre>
+        </div>
       </main>
     </div>
   );
